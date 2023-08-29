5bae986e7e9f82d40697cf0823cfda9c84032062
Chetan Navarkhele <chetannavarkhele@gmail.com>
2023-08-29 15:07:34 +0530

    commit 8

--- src/Login.js
+++ src/Login.js
@@ -13,11 +13,11 @@ const Login = () => {
         e.preventDefault();
         if (validate()) {
             // console.log('proceed further');
-            fetch("https://leave-database-be372-default-rtdb.asia-southeast1.firebasedatabase.app/users.json" + id).then((res) => {
+            fetch("https://leave-database-be372-default-rtdb.asia-southeast1.firebasedatabase.app/users/" + id +".json").then((res) => {
                 return res.json();
             }).then((resp) => {
                 console.log(resp);
-                
+
                 sessionStorage.setItem("firstName", resp.firstName);
                 sessionStorage.setItem("lastName", resp.lastName);
                 if (Object.keys(resp).length === 0) {
